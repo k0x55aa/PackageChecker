c84facd8f76f7f9563b55a5f7ff2f391378dba75
test <test@test.com>
2025-04-10 12:07:42 -0400

    cli added info

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,23 @@
 mod utils;
 use clap::Parser;
+
 #[derive(Parser, Debug)]
-#[command(name = "dependencyCheck", version, about = "")]
+#[command(name = "PackageChecker", version="0.1", about = "It checks for package in npm public repo")]
 struct ArgumentCli {
+    /// path to the package.json file
     #[arg(short, long, default_value = "")]
     filepath: String,
+    /// package name eg: npm
     #[arg(short, long, default_value = "")]
     package: String,
 }
-#[tokio::main]
 
+
+#[tokio::main]
 async fn main(){
     let args = ArgumentCli::parse();
     if args.package == "npm"{
         utils::npm::NpmCheck(&args.filepath).await;
     }
+
 }
